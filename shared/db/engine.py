1202548a82cfc175bf2b668ce8ae8e1310fca345
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-04 11:22:01 +0200

    #5: allow to inject DB config through env vars

--- shared/db/engine.py
+++ shared/db/engine.py
@@ -13,27 +13,28 @@ from mongoengine import (
     disconnect,
 )
 
-DEFAULT_PROTOCOL = "mongodb://"
-DEFAULT_HOST = "mongodb-service"
+DOCKER_DB_HOST = "mongodb-service"
+DB_HOST = os.environ.get("MONGO_HOST", DOCKER_DB_HOST)
+
+DB_PORT = int(os.environ.get("MONGO_PORT", 27017))
 
 DB_NAME = "krakendb"
-DB_PORT = int(os.environ.get("MONGO_PORT", 27017))
 
 
 USER = os.environ.get("MONGO_USER")
 PASSWORD = os.environ.get("MONGO_PASSWORD")
 
 
-def connect_db(host: str = DEFAULT_HOST, port: int = DB_PORT) -> None:
+def connect_db() -> None:
     """Connect to the database."""
     try:
         disconnect()
-        logging.info(f"Connecting to db: {host=} {DB_NAME=} {port=} {USER=}")
+        logging.info(f"Connecting to db: {DB_HOST=} {DB_NAME=} {DB_PORT=} {USER=}")
 
         connect(
             DB_NAME,
-            host=host,
-            port=port,
+            host=DB_HOST,
+            port=DB_PORT,
             username=USER,
             password=PASSWORD,
             authentication_source=DB_NAME,

d51b08c720c1104827f1aae79753e480a262ed25
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-11 20:29:20 +0200

    #24: show metrics in webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,22 +1,24 @@
 """A very simple Streamlit app that connects to a MongoDB database and displays the data from the RawFile collection."""
+# ruff: noqa: PD002 inplace=True should be avoided
 
 from collections import defaultdict
 
 import pandas as pd
 import streamlit as st
 
-from shared.db.engine import RawFile, connect_db
+from shared.db.engine import Metrics, RawFile, connect_db
 
 """
 # Welcome to AlphaKraken!
-Here's your data:
 """
 
 connect_db()
 
 st.write(f"Processed {RawFile.objects.count()} raw files.")
 
+# TODO: this throwaway code!
 
+# get raw file data
 raw_file_data = defaultdict(list)
 for raw_file in RawFile.objects.order_by("-created_at"):
     raw_file_data["name"].append(raw_file.name)
@@ -26,4 +28,25 @@ for raw_file in RawFile.objects.order_by("-created_at"):
     raw_file_data["instrument_id"].append(raw_file.instrument_id)
 
 raw_file_data_df = pd.DataFrame(raw_file_data)
-my_table = st.dataframe(raw_file_data_df)
+
+# get metrics
+metrics_data = defaultdict(list)
+for metric in Metrics.objects().order_by("-created_at"):
+    for k, v in metric.to_mongo().items():
+        metrics_data[k].append(v)
+
+metrics_data_df = pd.DataFrame(metrics_data)
+metrics_data_df.drop_duplicates(subset=["raw_file"], keep="last", inplace=True)
+metrics_data_df.drop(
+    columns=[
+        "_id",
+        "db_entry_created_at",
+    ],
+    inplace=True,
+)
+metrics_data_df.rename(columns={"raw_file": "name"}, inplace=True)
+
+# merge
+merged_df = raw_file_data_df.merge(metrics_data_df, on="name", how="left")
+
+my_table = st.dataframe(merged_df)

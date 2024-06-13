3b030b961e6ab55d45b93695b20a65f015e90520
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-13 16:38:24 +0200

    #30: add tests

--- webapp/service/components.py
+++ webapp/service/components.py
@@ -4,11 +4,11 @@ import pandas as pd
 import streamlit as st
 
 
-def show_filter(df: pd.DataFrame, filter_text: str) -> pd.DataFrame:
-    """Filter the DataFrame on textual comparison in all columns."""
-    user_input = st.text_input(filter_text, None)
+def show_filter(df: pd.DataFrame, text_to_display: str) -> pd.DataFrame:
+    """Filter the DataFrame on user input by case-insensitive textual comparison in all columns."""
+    user_input = st.text_input(text_to_display, None)
     if user_input is not None:
         user_input = user_input.lower()
-        mask = df.applymap(lambda x: user_input in str(x).lower()).any(axis=1)
+        mask = df.map(lambda x: user_input in str(x).lower()).any(axis=1)
         return df[mask]
     return df

92899c54d51bb21a7e1467348558795a396d04cb
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-13 17:00:30 +0200

    #30: show matches for filter

--- webapp/service/components.py
+++ webapp/service/components.py
@@ -7,9 +7,10 @@ import streamlit as st
 def show_filter(df: pd.DataFrame, text_to_display: str) -> pd.DataFrame:
     """Filter the DataFrame on user input by case-insensitive textual comparison in all columns."""
     user_input = st.text_input(text_to_display, None)
-    if user_input is not None:
+    if user_input is not None and user_input != "":
         user_input = user_input.lower()
         mask = df.map(lambda x: user_input in str(x).lower()).any(axis=1)
         mask |= df.index.map(lambda x: user_input in str(x).lower())
+        st.write(f"Found {mask.sum()} matches")
         return df[mask]
     return df

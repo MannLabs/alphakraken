68a1af26d431935d666f9b90528c58595cb9ee74
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-06-20 16:38:16 +0200

    CHORE: fix shared env vars

--- airflow_src/plugins/common/keys.py
+++ airflow_src/plugins/common/keys.py
@@ -90,15 +90,10 @@ class QuantingEnv:
 
 
 class EnvVars:
-    """Keys for accessing docker environmental variables."""
+    """Keys for accessing docker environmental variables for the airflow part."""
 
     INSTRUMENT_PATH_TEST1 = "INSTRUMENT_PATH_TEST1"
     INSTRUMENT_PATH_ASTRAL1 = "INSTRUMENT_PATH_ASTRAL1"
     INSTRUMENT_PATH_ASTRAL2 = "INSTRUMENT_PATH_ASTRAL2"
-
-    MONGO_HOST = "MONGO_HOST"
-    MONGO_PORT = "MONGO_PORT"
-    MONGO_USER = "MONGO_USER"
-    MONGO_PASSWORD = "MONGO_PASSWORD"  # noqa: S105  #Possible hardcoded password
 
     IO_POOL_FOLDER = "IO_POOL_FOLDER"

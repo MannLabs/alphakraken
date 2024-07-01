1fe73fd4ccb1dba9dbc470cfd55e4fdce5867265
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-07-01 17:42:59 +0200

    #64: fix error handling for copying

--- airflow_src/plugins/callbacks.py
+++ airflow_src/plugins/callbacks.py
@@ -3,7 +3,7 @@
 import logging
 from typing import Any
 
-from common.keys import XComKeys
+from common.keys import DagContext, DagParams, XComKeys
 from common.utils import get_xcom
 
 from shared.db.interface import update_raw_file
@@ -21,12 +21,15 @@ def on_failure_callback(context: dict[str, Any], **kwargs) -> None:
     logging.info(f"task {ti.task_id} failed in dag {ti.dag_id} ")
 
     try:
-        raw_file_name = get_xcom(ti, key=XComKeys.RAW_FILE_NAME)
+        raw_file_name = context[DagContext.PARAMS][DagParams.RAW_FILE_NAME]
     except KeyError:
-        logging.warning(
-            "could not find raw file name in xcom. Not updating status in db."
-        )
-        return
+        try:
+            raw_file_name = get_xcom(ti, key=XComKeys.RAW_FILE_NAME)
+        except KeyError:
+            logging.warning(
+                "could not find raw file name in dag params nor xcom. Not updating status in db."
+            )
+            return
 
     ex = context["exception"]
 

12d269b780b84c4ce0dcd6f7e914f34b1d45a949
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-07-04 16:34:39 +0200

    #68: renaming

--- airflow_src/plugins/sensors/file_sensor.py
+++ airflow_src/plugins/sensors/file_sensor.py
@@ -10,13 +10,7 @@ from common.settings import (
     get_internal_backup_path,
     get_internal_instrument_data_path,
 )
-from watchdog.events import FileSystemEvent, FileSystemEventHandler
-
-# Over the network, and on some Mac/Docker configurations,
-# the dynamically chosen Observer does not work.
-# TODO: fix this or get rid of watchdog again
-# from watchdog.observers import Observer
-from watchdog.observers.polling import PollingObserver as Observer
+from raw_data_wrapper import RawDataWrapper
 
 from shared.db.interface import update_kraken_status
 from shared.db.models import KrakenStatusValues
@@ -42,19 +36,6 @@ def _check_health(instrument_id: str) -> None:
     )
 
 
-class FileCreationEventHandler(FileSystemEventHandler):
-    """Event handler for file creation."""
-
-    def __init__(self) -> None:
-        """Initialize the event handler."""
-        self.file_created = False
-
-    def on_created(self, event: FileSystemEvent) -> None:
-        """Set flag on file creation."""
-        del event  # unused
-        self.file_created = True
-
-
 class FileCreationSensor(BaseSensorOperator):
     """Sensor to check for file creation."""
 
@@ -62,27 +43,22 @@ class FileCreationSensor(BaseSensorOperator):
         """Initialize the sensor."""
         super().__init__(*args, **kwargs)
 
-        self._instrument_id = instrument_id
-        self._path_to_watch = get_internal_instrument_data_path(instrument_id)
-        logging.info(f"Creating FileCreationSensor for {self._path_to_watch}")
+        self._raw_data_wrapper = RawDataWrapper.create(
+            instrument_id=instrument_id, raw_file_name=None
+        )
 
-        self._event_handler = FileCreationEventHandler()
-        self._observer = None
+        self._instrument_id = instrument_id
+        self._initial_dir_contents = None
+
+    def pre_execute(self, context: dict[str, any]) -> None:
+        """Check the health of the instrument data path and backup path."""
+        del context  # unused
+
+        self._initial_dir_contents = self._raw_data_wrapper.get_dir_contents()
 
     def poke(self, context: dict[str, any]) -> bool:
         """Check if file was created. If so, push the folder contents to xcom and return."""
         del context  # unused
-
-        if self._observer is None:
-            logging.info("self.observer is None")
-            self._observer = Observer()
-
-        if not self._observer.is_alive():
-            logging.info("not self.observer.is_alive()")
-            self._observer.schedule(
-                self._event_handler, str(self._path_to_watch), recursive=False
-            )
-            self._observer.start()
 
         logging.info(
             f"Checking for new files since start of this DAG run in {self._path_to_watch}"
@@ -90,12 +66,11 @@ class FileCreationSensor(BaseSensorOperator):
 
         _check_health(self._instrument_id)
 
-        if self._event_handler.file_created:
-            logging.info("self.event_handler.file_created()")
-
-            self._observer.stop()
-            self._observer.join()
-
+        if (
+            new_content := self._raw_data_wrapper.get_dir_contents()
+            - self._initial_dir_contents
+        ):
+            logging.info(f"got new dir_content {new_content}")
             return True
 
         return False

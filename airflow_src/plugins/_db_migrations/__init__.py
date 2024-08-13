4de52351a7fbed1b77a2ecfe70b6f0bce0b49f10
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-13 11:16:16 +0200

    #112: add DB migrations

---
+++ airflow_src/plugins/_db_migrations/__init__.py
@@ -0,0 +1,12 @@
+"""Module containing database migrations.
+
+These are one-time non-trivial modifications to the database to update old data to schema changes.
+
+Needs to be executed manually by a developer.
+If you're not sure what you're doing, don't do it.
+
+!!!! IMPORTANT !!!!
+!!!! Make sure to backup the database before running any of these scripts!
+!!!! Make sure to stop all worker tasks before running these scripts to avoid conflicts.
+!!!!!!!!!!!!!!!!!!
+"""

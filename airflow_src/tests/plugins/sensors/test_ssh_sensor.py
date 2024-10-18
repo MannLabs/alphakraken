96f75ee53f187cec72bb00f6b0d06a98deb90272
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-10-18 22:23:22 +0200

    fix tests

--- airflow_src/tests/plugins/sensors/test_ssh_sensor.py
+++ airflow_src/tests/plugins/sensors/test_ssh_sensor.py
@@ -125,7 +125,7 @@ def test_ssh_execute_multiple_tries(mock_sleep: MagicMock) -> None:
 
     # then
     assert result == "command output"
-    assert mock_sleep.call_count == 4  # noqa: PLR2004
+    assert mock_sleep.call_count == 5  # noqa: PLR2004
     assert ssh_hook.exec_ssh_client_command.call_count == 5  # noqa: PLR2004
 
     expected_call = call(

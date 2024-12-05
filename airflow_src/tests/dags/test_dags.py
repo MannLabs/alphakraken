2856b767ef8785bf863a50e2164dc35fda5b7825
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-12-05 14:09:33 +0100

    fix tests

--- airflow_src/tests/dags/test_dags.py
+++ airflow_src/tests/dags/test_dags.py
@@ -77,3 +77,14 @@ def test_dag_load_file_mover(dagbag: DagBag) -> None:
     assert dagbag.import_errors == {}
     assert dag is not None
     assert len(dag.tasks) == 2  # noqa: PLR2004 no magic numbers
+
+
+def test_dag_load_file_remover(dagbag: DagBag) -> None:
+    """Test that file_remover loads correctly."""
+    # when
+    dag = dagbag.get_dag(dag_id="file_remover")
+
+    # then
+    assert dagbag.import_errors == {}
+    assert dag is not None
+    assert len(dag.tasks) == 2  # noqa: PLR2004 no magic numbers

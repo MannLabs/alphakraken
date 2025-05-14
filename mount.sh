6dccdc96df4248af737b792f708717cd1724e908
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-05-14 12:24:55 +0200

    rewrite docs on mounting

--- mount.sh
+++ mount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # A little helper for mounting.
-# Will read data from environmental-specific alphakraken.yaml file and do the mounting.
+# Will read data from environmental-specific alphakraken.yaml file and do the mounting or create fstab entries.
 
 # IMPORTANT NOTE: it is absolutely crucial that the mounts are set correctly in the respective alphakraken.yaml file!
 # Make sure the data (user names, ip addresses) are always up to date!
 # If you need to change this script, do so with caution and test it on a sandbox first!
 
 # This could be used to loop over all instruments:
-# for i in $(python -c "import yaml, sys; print(' '.join(yaml.safe_load(open(sys.argv[1]))[sys.argv[2]].keys()))" envs/alphakraken.${ENV}.yaml instruments); do echo $i; done
+# for i in $(python3 -c "import yaml, sys; print(' '.join(yaml.safe_load(open(sys.argv[1]))[sys.argv[2]].keys()))" envs/alphakraken.${ENV}.yaml instruments); do ./mount.sh $i; done
 
 if [ "$ENV" == "" ]; then
   echo "Please set the ENV variable, e.g. 'export ENV=local' (or 'export ENV=sandbox', 'export ENV=production')"
@@ -18,18 +18,21 @@ fi
 set -e -u
 
 if [ -z "${1:-}" ] ; then
-  echo "Usage: $0 <entity> [umount]"
+  echo "Usage: $0 <entity> [fstab|mount|umount]"
   echo "<entity> can be an instrument name (e.g. test1, ..) or a special folder (logs, backup, or output)."
-  echo "If 'unmount' is passed, the target folder will be unmounted first."
+  echo "If 'fstab' is passed, an entry for the /etc/fstab file will be created."
+  echo "If 'mount' is passed, the source folder will be mounted to the target folder."
+  echo "If 'umount' is passed, the target folder will be unmounted first, before mounting the source folder to the target folder."
   echo
-  echo "Example 1: $0 test1"
-  echo "Example 2: $0 backup umount"
+  echo "Example 1: $0 logs fstab"
+  echo "Example 2: $0 test1 mount"
+  echo "Example 3: $0 backup umount"
   exit 1
 fi
 
 # arguments:
 ENTITY=$1 # instrument name as defined in alphakraken.yaml, e.g. test1
-DO_UMOUNT=${2:-}  # set to "umount" for unmounting first
+ACTION=${2:-fstab}  # set to "umount" for unmounting first, "fstab" for generating entry
 
 FILE_NAME=envs/alphakraken.${ENV}.yaml
 if [ ! -f $FILE_NAME ]; then
@@ -54,16 +57,19 @@ fi
 
 MOUNTS_PATH=$(get_data locations general mounts_path)
 USERNAME=$(get_data $ENTITY_TYPE $ENTITY username)
-PASSWORD=$(get_data $ENTITY_TYPE $ENTITY password) || echo Password not found, needs to be entered manually
 MOUNT_SRC="$(get_data $ENTITY_TYPE $ENTITY mount_src)"
 MOUNT_TARGET="$(get_data $ENTITY_TYPE $ENTITY mount_target)"
 
 MOUNT_TARGET=$MOUNTS_PATH/$MOUNT_TARGET
 
+if [ "${ACTION}" == "fstab" ]; then
+  echo "${MOUNT_SRC// /\\040}" "${MOUNT_TARGET// /\\040}" cifs username=$USERNAME,password=SET_PASSWORD,uid=$(id -u),gid=$(id -g) 0 0
+  exit 0
+fi
+
 echo MOUNT_SRC=$MOUNT_SRC
 echo MOUNT_TARGET=$MOUNT_TARGET
 echo USERNAME=$USERNAME
-echo PASSWORD=$PASSWORD
 echo ENV=$ENV
 
 echo "Are you sure you want to continue? (ENTER to continue, CTRL-C to abort)"
@@ -86,7 +92,7 @@ fi
 
 echo mounting "$MOUNT_SRC" to "$MOUNT_TARGET"
 
-if [ "${DO_UMOUNT}" == "umount" ]; then
+if [ "${ACTION}" == "umount" ]; then
   if [ $(isMounted $MOUNT_TARGET) == 0 ]; then
     echo "Nothing to unmount, $MOUNT_TARGET is not mounted."
     exit
@@ -96,11 +102,7 @@ if [ "${DO_UMOUNT}" == "umount" ]; then
 fi
 
 # pass user id (uid) and group id (gid) otherwise would be mounted as root
-if [ -n "$PASSWORD" ]; then
-  sudo mount -t cifs -o username=$USERNAME,password=$PASSWORD,uid=$(id -u),gid=$(id -g) "$MOUNT_SRC" "$MOUNT_TARGET"
-else
-  sudo mount -t cifs -o username=$USERNAME,uid=$(id -u),gid=$(id -g) "$MOUNT_SRC" "$MOUNT_TARGET"
-fi
+sudo mount -t cifs -o username=$USERNAME,uid=$(id -u),gid=$(id -g) "$MOUNT_SRC" "$MOUNT_TARGET"
 
 echo contents of  "$MOUNT_TARGET":
 ls "$MOUNT_TARGET"

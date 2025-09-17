fad20cb3106c559b452021426cf87359b236cb8b
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-09-17 21:49:31 +0200

    feat: add job status handling to components and update warning display logic

--- webapp/service/status.py
+++ webapp/service/status.py
@@ -3,13 +3,16 @@
 import streamlit as st
 from service.db import df_from_db_data, get_status_data
 
-from shared.db.models import KrakenStatusValues
+from shared.db.models import KrakenStatusEntities, KrakenStatusValues
 
 
 def display_status_warning() -> None:
     """Display a warning if there are instruments with issues."""
     status_data_df = df_from_db_data(get_status_data())
-    nok_status_df = status_data_df[status_data_df["status"] != KrakenStatusValues.OK]
+    nok_status_df = status_data_df[
+        (status_data_df["entity_type"] == KrakenStatusEntities.INSTRUMENT)
+        & (status_data_df["status"] != KrakenStatusValues.OK)
+    ]
     if len(nok_status_df):
         instruments_with_issues = nok_status_df["_id"].unique()
         st.warning(

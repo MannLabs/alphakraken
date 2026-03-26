16678af9e630f2992a3c35b6f1138b33a07d9a95
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-03-26 13:27:46 +0100

    add peptide count metrics for heavy and light peptides

--- airflow_src/plugins/metrics/metrics/skyline.py
+++ airflow_src/plugins/metrics/metrics/skyline.py
@@ -65,6 +65,25 @@ def _sanitize_peptide_name(sequence: str) -> str:
     return sequence.replace("[", "_").replace("]", "").replace(".", ":")
 
 
+class PeptideCountStats(Metrics):
+    """Number of detected heavy/light peptides."""
+
+    _file = SKYLINE_REPORT_FILE_NAME
+    _columns: ClassVar = {}
+
+    def _calc_metrics(self) -> None:
+        df = self._data_store[self._file]
+        self._metrics["irt_n_heavy_peptides"] = len(
+            df[df["IsotopeLabelType"] == "heavy"]
+        )
+        self._metrics["irt_n_light_peptides"] = len(
+            df[df["IsotopeLabelType"] == "light"]
+        )
+
+    def _calc(self, df: pd.DataFrame, source_column: str, target_column: str) -> None:
+        pass
+
+
 class PerPeptideStats(Metrics):
     """Per-peptide metrics (heavy isotopes only)."""
 
@@ -97,6 +116,7 @@ def calc_skyline_metrics(output_directory: Path) -> dict[str, str | int | float]
 
     metrics = RetentionTimeStats(data_store).get()
     metrics |= AreaStats(data_store).get()
+    metrics |= PeptideCountStats(data_store).get()
     metrics |= PerPeptideStats(data_store).get()
 
     return metrics

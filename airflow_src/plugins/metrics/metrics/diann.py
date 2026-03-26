821d1f02c6fe2d01a210ea714db1c04a95152bd6
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-03-26 17:29:23 +0100

    add simple diann metrics

---
+++ airflow_src/plugins/metrics/metrics/diann.py
@@ -0,0 +1,35 @@
+"""Metrics for DIA-NN."""
+
+from pathlib import Path
+from typing import ClassVar
+
+import pandas as pd
+from metrics.metrics.base import DataStore, Metrics, read_tsv
+
+REPORT_FILE_NAME = "report.tsv"
+
+file_name_to_read_method_mapping = {
+    REPORT_FILE_NAME: read_tsv,
+}
+
+
+class BasicStats(Metrics):
+    """Basic statistics from DIA-NN report."""
+
+    _file = REPORT_FILE_NAME
+
+    _columns: ClassVar[dict[str, str | None]] = {
+        "Protein.Names": "proteins",
+        "N.Sequences": "peptides",
+    }
+
+    def _calc(self, df: pd.DataFrame, source_column: str, target_column: str) -> None:
+        """Calculate metrics by counting unique values."""
+        self._metrics[target_column] = df[source_column].nunique()
+
+
+def calc_diann_metrics(output_directory: Path) -> dict[str, str | int | float]:
+    """Calculate DIA-NN metrics."""
+    data_store = DataStore(output_directory, file_name_to_read_method_mapping)
+
+    return BasicStats(data_store).get()

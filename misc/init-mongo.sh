3f48b0b6ad2a37eac1d7a0efb32cd0688c69f506
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-06-10 17:48:56 +0200

    adapt init-mongo.sh

--- misc/init-mongo.sh
+++ misc/init-mongo.sh
@@ -38,6 +38,10 @@ mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" admin 
                 resource: { db: "krakendb", collection: "project_settings" },
                 actions: ["find", "insert", "update", "remove"]
             },
+            {
+              resource: { db: "krakendb", collection: "user" },
+              actions: ["find", "insert", "update", "remove"]
+            },
         ],
         roles: ["read"]
     });

91a3b0e4c4e24dda28888037e3ed317c83ce3759
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-08-14 12:34:28 +0200

    Revert "add handling for file disappearance and renaming in acquisition monitoring"
    This reverts commit c3687c8b747f981bc650833f2b963d7a02c88c66.

--- shared/db/models.py
+++ shared/db/models.py
@@ -93,9 +93,6 @@ class InstrumentFileStatus:
     NA = "n/a"  # for backwards compatibility
     INITIAL = "initial"  # File exists on instrument, not yet moved
     RENAMED = "renamed"  # File was renamed on instrument (read as "file does no longer exist under its name as it was renamed")
-    DISAPPEARED = (
-        "disappeared"  # File vanished from the instrument without being renamed
-    )
     MOVED = "moved"  # File moved from instrument to instrument backup folder
     PURGED = "purged"  # File removed from instrument backup folder
 

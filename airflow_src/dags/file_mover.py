2c5348884f1ccfcdd5b34fac657cecb061875b12
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-08-26 09:19:25 +0200

    airflow 3: add apache-airflow-providers-standard

--- airflow_src/dags/file_mover.py
+++ airflow_src/dags/file_mover.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 from airflow.models import Param
 from airflow.models.dag import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from common.constants import (
     AIRFLOW_QUEUE_PREFIX,
 )

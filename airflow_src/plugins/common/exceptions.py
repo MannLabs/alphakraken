4338f7b76d62096459ad5a0695cb51b076dd4adc
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-09-16 06:05:58 +0200

    add TODO

--- airflow_src/plugins/common/exceptions.py
+++ airflow_src/plugins/common/exceptions.py
@@ -2,6 +2,7 @@
 
 from airflow.exceptions import AirflowFailException, AirflowSkipException
 
+# TODO move all custom exceptions here
 
 class QuantingFailedNewErrorException(AirflowFailException):
     """Raise if quanting failed with a new error."""
